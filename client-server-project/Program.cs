cbd27a83fdc67114ae1cfd6102eb9818a021ec23
Matheus Batista <120420165+matheustsx@users.noreply.github.com>
2024-06-10 23:35:00 -0300

    initial commit

---
+++ client-server-project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client_server_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste");
+            Console.ReadLine();
+        }
+    }
+}
