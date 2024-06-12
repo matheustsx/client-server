34268ddee1a945506cf3cf026fe2f3f760aa07df
matheustsx <matheusbatistachavante@gmail.com>
2024-06-11 21:46:04 -0300

    feat: teste de funcionamento de uma thread

--- client-server-project/Program.cs
+++ client-server-project/Program.cs
@@ -1,17 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace client_server_project
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Teste");
+            Thread myThread = new Thread(newThread);
+            myThread.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Thread 1: " + i);
+                Thread.Sleep(500);
+            }
+            myThread.Join();
+            Console.WriteLine("Terminou viu meu fi");
             Console.ReadLine();
+        }
+        static void newThread()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Thread 2: " + i);
+                Thread.Sleep(500);
+            }
         }
     }
 }
